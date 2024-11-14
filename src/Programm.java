a07657fe4d98c0372f16f93db67d47594b41fc91
Alekseika <lvovnanatala05@gmail.com>
2024-11-14 13:25:05 +0400

    сделал команду help

--- src/Programm.java
+++ src/Programm.java
@@ -13,18 +13,25 @@ public class Programm {
                     infinity = false;
                     break;
                 }
-                case "help"{
+                case "help": {
                     System.out.println("Список команд: ");
                     System.out.println("-----------------");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
-                    System.out.println("Список команд: ");
+                    System.out.println();
+                    System.out.println("creat_man: команда создает человека, (экземпляр класса Man)");
+                    System.out.println("kill_man: команда убивает человека");
+                    System.out.println("talk: команда заставляет человека говорить (если создан экземпляр класса)");
+                    System.out.println("go: команда заставляет человека идти (если создан экземпляр класса)");
+                    System.out.println("isalive: команда проверяет, жив ли человек (если создан экземпляр класса)");
+                    System.out.println();
+                    System.out.println("----------------");
+                    System.out.println("----------------");
+                    break;
+                }
+                case "create_man": {
+                    if (someman != null ){
+                        someman.kill();
+                        System.out.println("Человек");
+                    }
                 }
             }
         }
